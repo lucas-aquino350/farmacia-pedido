b324a23eed09ade555ebbb73975a6880535d378f
Lucas <lucasaquino350@gmail.com>
2023-01-17 17:46:03 -0300

    implementa api de teste

--- src/main/java/br/com/farnaciaContinental/farmaciapedido/FarmaciaPedidoApplication.java
+++ src/main/java/br/com/farnaciaContinental/farmaciapedido/FarmaciaPedidoApplication.java
@@ -2,12 +2,18 @@ package br.com.farnaciaContinental.farmaciapedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FarmaciaPedidoApplication {
-
+    @GetMapping
+	public String getHomeTeste() {
+		return "API FARMACIA-PEDIDO  - TESTE";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FarmaciaPedidoApplication.class, args);
 	}
-
 }
