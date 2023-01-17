97fc1a64cca8bcfc1b8e281dd08e2241653ffc03
Lucas <lucasaquino350@gmail.com>
2023-01-17 18:22:35 -0300

    implementa request e response

--- src/main/java/br/com/farnaciaContinental/farmaciapedido/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/farnaciaContinental/farmaciapedido/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,25 @@
 package br.com.farnaciaContinental.farmaciapedido.cliente.application.api;
 
+import java.time.LocalDateTime;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+import br.com.farnaciaContinental.farmaciapedido.cliente.domain.Sexo;
+import lombok.Value;
+@Value
 public class ClienteRequest {
-
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@CPF
+	private String cpf;
+	@Email
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String celular;
+	@NotNull
+	private LocalDateTime dataDeNascimento;
+	private Sexo sexo;
 }
