a305a62051f666ab217f8fc68557baa78ef820fe
Lucas <lucasaquino350@gmail.com>
2023-01-17 17:23:57 -0300

    inicia projeto

---
+++ src/main/java/br/com/farnaciaContinental/farmaciapedido/FarmaciaPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.farnaciaContinental.farmaciapedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/farnaciaContinental/farmaciapedido/FarmaciaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farnaciaContinental.farmaciapedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
